76862d9af355ecaecfd4d0d0b0ea2c1fe7d7a811
AetherX-222 <aetherx222@gmail.com>
2025-02-15 14:38:58 +0530

    `feat: Added Clerk webhook secret and user controller function to handle Clerk user events`

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 //APP CONFIG
@@ -19,7 +20,9 @@ app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})   
+}) 
+
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
